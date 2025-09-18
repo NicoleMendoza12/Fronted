24d5cdd257ca56758b8760097cdcf02dd102cd25
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-09-18 17:16:00 -0300

    Agregar archivos de proyecto.

---
+++ FrontAuth.WebApp/Program.cs
@@ -0,0 +1,50 @@
+using FrontAuth.WebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddHttpClient<ApiService>(client =>
+{
+    // Define la URL base de la API que vamos a consumir
+    client.BaseAddress = new Uri("https://localhost:7280/api/"); // API base (puerto cambia segun pc)
+});
+
+builder.Services.AddScoped<AuthService>();
+
+//configuración de la autenticación de la aplicación usando cookies
+builder.Services.AddAuthentication("AuthCookie")
+    .AddCookie("AuthCookie", options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // Después de 60 minutos, el usuario tendrá que iniciar sesión nuevamente
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+ 
